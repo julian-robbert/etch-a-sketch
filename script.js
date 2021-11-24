4f56d4eace926af5c7ed1ce5964a4ce18d42911e
Julian Robbert <julian.robbert@gmail.com>
2021-11-23 16:38:31 -0800

    finished logic and UI, including buttons to reset the grid, clear the user's drawing, and set the grid size

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 let gridContainer = document.getElementById('gridContainer');
 
-let gridSize = 16;
+//fills grid with appropriate amount of divs that can be hovered to change colour
+function makeSquares(gridSize){
+    for (i=0;i<gridSize*gridSize;i++){
+        let drawSquare = document.createElement('div');
+        drawSquare.classList.add('drawSquare');
+        function colorChange(item){
+            item.style.backgroundColor = '#424242';
+        }
+        function whiteToBlack(){
+            colorChange(drawSquare);
+        }
+        drawSquare.addEventListener("mouseover", whiteToBlack);
+        gridContainer.append(drawSquare);
+    }
+}
+
+//defines the standard 16x16 grid and then calls it when the page loads
+function defaultGrid(){
+    gridSize = 16;
+    let r = document.querySelector(':root');
+    r.style.setProperty('--rows', gridSize);
+    r.style.setProperty('--columns', gridSize);
+    makeSquares(gridSize);
+}
+
+defaultGrid();
+
+//prompts user for new grid size then changes grid accordingly
 function promptGridSize(){
     gridContainer.innerHTML = "";
     gridSize = prompt("Please provide a grid size between 1 and 100");
@@ -13,63 +40,17 @@ function promptGridSize(){
     r.style.setProperty('--columns', gridSize);
     console.log(gridSize);
 
-    for (i=0;i<gridSize*gridSize;i++){
-        let drawSquare = document.createElement('div');
-        drawSquare.classList.add('drawSquare');
-        //drawSquare.style.border = '1px solid black';
-        function colorChange(item){
-            item.style.backgroundColor = '#424242';
-        }
-        function whiteToBlack(){
-            colorChange(drawSquare);
-        }
-        drawSquare.addEventListener("mouseover", whiteToBlack);
-        gridContainer.append(drawSquare);
-    }
+    makeSquares(gridSize);
 }
 
-function clear(){
+//clears the drawing while preserving current grid size
+function clearDrawing(){
     gridContainer.innerHTML = "";
-    for (i=0;i<gridSize*gridSize;i++){
-        let drawSquare = document.createElement('div');
-        drawSquare.classList.add('drawSquare');
-        //drawSquare.style.border = '1px solid black';
-        function colorChange(item){
-            item.style.backgroundColor = '#424242';
-        }
-        function whiteToBlack(){
-            colorChange(drawSquare);
-        }
-        drawSquare.addEventListener("mouseover", whiteToBlack);
-        gridContainer.append(drawSquare);
-    }
+    makeSquares(gridSize);
 }
 
+//clears drawing and sets grid back to default size
 function resetGrid(){
     gridContainer.innerHTML = "";
     defaultGrid();
 }
-
-
-function defaultGrid(){
-    gridSize = 16;
-    let r = document.querySelector(':root');
-    r.style.setProperty('--rows', gridSize);
-    r.style.setProperty('--columns', gridSize);
-    for (i=0;i<gridSize*gridSize;i++){
-        let drawSquare = document.createElement('div');
-        drawSquare.classList.add('drawSquare');
-        //drawSquare.style.border = '1px solid black';
-        function colorChange(item){
-            item.style.backgroundColor = '#424242';
-        }
-        function whiteToBlack(){
-            colorChange(drawSquare);
-        }
-        drawSquare.addEventListener("mouseover", whiteToBlack);
-        gridContainer.append(drawSquare);
-    }
-}
-defaultGrid();
-
-
